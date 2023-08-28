e633cfb34c385cdd8901e209fbb17174d382a9d8
hseungho <hsh47607@gmail.com>
2023-08-28 21:22:22 +0900

    first commit

---
+++ src/main/java/com/example/fosslightworkflowtest/FosslightWorkflowTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.fosslightworkflowtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosslightWorkflowTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FosslightWorkflowTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fosslightworkflowtest/FosslightWorkflowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fosslightworkflowtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosslightWorkflowTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
